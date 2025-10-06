1dbbac192f3a50cd064a0962c1858b6985b7eee2
zhangdayeb <you@example.com>
2025-10-06 09:55:16 +0800

    'up'

--- route/app.php
+++ route/app.php
@@ -22,7 +22,6 @@ Route::get('hello/:name', 'index/hello');
 Route::rule('caiji/vendors', 'caiji.GameVendors/get_vendors');                  // 获取厂商列表 更新的时候用一次
 Route::rule('caiji/list', 'caiji.GameList/get_list');                           // 厂商 游戏列表 更新的时候用一次
 Route::rule('caiji/url', 'caiji.GameUrl/get_url');                              // 获取游戏列表 需要配合 用户余额执行 厂商会回调用户余额 暂时所有参数都是 url 传参
-Route::rule('caiji/terminate', 'caiji.GameTerminate/get_terminate');            // 通知厂商 终止 用户游戏
 Route::rule('caiji/down', 'caiji.GameDownload/get_down');                       // 从游戏接口下载图片
 Route::rule('caiji/transfer', 'caiji.GameTransfer/get_transfer');               // 游戏从采集转移到游戏表中
 
